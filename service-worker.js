7afe36aec0f15d3a2279356a24d1eb5fbcfa012b
zphysicaltherapyspecialist <zphysicaltherapyspecialist@gmail.com>
2025-11-21 12:05:46 -0500

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,48 @@
+// DrZ NeuroPT DME Catalog - Service Worker
+const CACHE_NAME = 'drz-dme-catalog-v1';
+const urlsToCache = [
+  './index.html',
+  './manifest.json'
+];
+
+// Install event - cache resources
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Fetch event - serve from cache, fallback to network
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
+
+// Activate event - clean up old caches
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
